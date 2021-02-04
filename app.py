a5620cd15b9a9d302c4b19f251c45fdddc4c8cd2
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-05 09:31:41 +1030

    comments

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from project import app
 
+# simply start point for the server
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- project/models.py
+++ project/models.py
@@ -3,9 +3,13 @@ from project import db
 class User(db.Model):
     __tablename__ = 'users'
 
+    # Structure of table
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Text)
     password = db.Column(db.Text)
+
+    # Getter and Setter?
 
     def __init__(self, username, password):
         self.username = username
