b6d466c13f8a5d358f1426032ca5da35f47147dc
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 09:36:34 +1030

    added template and blueprint for comparison pages

--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-from flask_modus import Modus
+# from flask_modus import Modus
 from flask_migrate import Migrate
 import os
 
@@ -20,23 +20,29 @@ migrate = Migrate(app, db)
 
 # method overides
 
-modus = Modus(app)
+# modus = Modus(app)
 
-# bluepring initialisations
+## blueprint initialisations
+
+# blueprint for user pages
 
 from project.users.views import users_blueprint
 app.register_blueprint(users_blueprint, url_prefix='/user')
 
-# blueprint for auth routes
+# blueprint for auth pages
 
 from project.auth.views import auth_blueprint
 app.register_blueprint(auth_blueprint)
 
-# blueprint for non-auth routes
+# blueprint for home page
 
 from project.home.views import home_blueprint
 app.register_blueprint(home_blueprint, url_prefix='/home')
 
+# blueprint for comparison page
+
+from project.compare.views import compare_blueprint
+app.register_blueprint(compare_blueprint, url_prefix='/compare')
 
 # root route
 
---
+++ project/comparison/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+
+
+compare_blueprint = Blueprint(
+    'compare',
+    __name__,
+    template_folder='templates'
+)
+
+@compare_blueprint.route('/', methods=["GET"])
+def home():
+    return render_template('compare.html')
