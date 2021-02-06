a63a2fe66cd51df20bef02a8a2f817b5e4f84899
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 21:31:35 +1030

    user profile working

--- project/users/controller.py
+++ project/users/controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
 from project.models import User, Movies
+import requests
 
 users_blueprint = Blueprint(
     'users',
