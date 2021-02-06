a364737555ee7b22c12f926f1364a367135ed178
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 14:34:39 +1030

    added movies to model

--- project/models.py
+++ project/models.py
@@ -1,6 +1,8 @@
 from project import db
 from flask_login import UserMixin
 
+# users table
+
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
 
@@ -15,3 +17,20 @@ class User(UserMixin, db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+# movies table
+
+class Movies(db.model):
+    __tablename__ = 'movies'
+
+    # Structure of table
+
+    id = db.column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+
+    # Getter and Setter
+
+    def __init__(self, movie_id, user_id):
+        self.movie_id = movie_id
+        self.user_id = user_id
