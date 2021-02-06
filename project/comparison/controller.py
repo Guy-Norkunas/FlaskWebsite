a5e235ebf3e41906ca21bd150e8d9ec845e6c5bc
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 20:12:48 +1030

    basic features for comparing with other users

--- project/comparison/controller.py
+++ project/comparison/controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 
@@ -11,4 +11,6 @@ compare_blueprint = Blueprint(
 @compare_blueprint.route('/', methods=["GET"])
 @login_required
 def index():
+    print(request.args.get('users'))
+
     return render_template('compare.html')
