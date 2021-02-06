dd44fcd6f9185f7eaac760c32223729fe8c58aff
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 20:21:18 +1030

    basic features for comparing with other users

--- project/comparison/controller.py
+++ project/comparison/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
-
+from project.models import Movies, User
 
 compare_blueprint = Blueprint(
     'compare',
@@ -13,4 +13,11 @@ compare_blueprint = Blueprint(
 def index():
     print(request.args.get('users'))
 
-    return render_template('compare.html')
+    user_movies = Movies.query.filter_by(user_id=current_user).all()
+    user = User.query.filter_by(username=request.args.get('users')).first()
+    other_movies = Movies.query.filter_by(user_id=user).all()
+
+    print(user_movies)
+    print(other_movies)
+
+    return render_template('compare.html', movies = )
