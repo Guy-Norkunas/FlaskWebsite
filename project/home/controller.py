c17fe2425a2cbd486b465828f005139669c2db84
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 16:38:16 +1030

    added cards to display movies

--- project/home/controller.py
+++ project/home/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import current_user
-import requests
+import requests, json
 
 home_blueprint = Blueprint(
     'home',
@@ -10,6 +10,7 @@ home_blueprint = Blueprint(
 
 @home_blueprint.route('/', methods=["GET"])
 def index():
-    r = requests.get('https://api.github.com/events')
-    print(r.text)
-    return render_template('home.html')
+
+    r = requests.get('https://api.themoviedb.org/3/discover/movie?api_key=cd082f86556318fbd6e151825ae40fc7&language=en-US&sort_by=popularity.desc&include_video=false&page=1')
+    
+    return render_template('home.html', movies=r.json()['results'])
