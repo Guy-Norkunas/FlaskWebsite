15b2d547c39fd96e497b356915731938349d44ad
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 18:22:19 +1030

    adding favourite button functionality. fixing movie display

--- project/movies/controller.py
+++ project/movies/controller.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
-from flask_login import current_user
+from flask import Blueprint, render_template, redirect, request
+from flask_login import current_user, login_required
+import requests
 
 movies_blueprint = Blueprint(
     'movies',
@@ -9,4 +10,13 @@ movies_blueprint = Blueprint(
 
 @movies_blueprint.route('/<id>', methods=["GET"])
 def movie(id):
-    return render_template('movie.html', movie=f"movie {id}")
+
+    r = requests.get(f"https://api.themoviedb.org/3/movie/{id}?api_key=cd082f86556318fbd6e151825ae40fc7&language=en-US")
+
+    return render_template('movie.html', movie=r.json())
+
+@movies_blueprint.route('/<id>', methods=["POST"])
+@login_required
+def favourite(id):
+    print(request.args.get('url'))
+    return redirect(request.args.get('url'))
