51f586dc4312f5111a2ce86e504a16896cd56e7c
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 11:05:03 +1030

    formatting/comments

--- project/auth/controller.py
+++ project/auth/controller.py
@@ -6,22 +6,36 @@ auth_blueprint = Blueprint(
     template_folder='templates'
 )   
 
+## login routes
+
+# get
+
 @auth_blueprint.route('/login', methods=["GET"])
 def login_get():
     return render_template('login.html')
+
+# post
 
 @auth_blueprint.route('/login', methods=["POST"])
 def login_post():
     return redirect(url_for('home.index'))
 
-@auth_blueprint.route('/logout', methods=["GET"])
-def logout():
-    return render_template('logout.html')
+## signup routes
+
+# get
 
 @auth_blueprint.route('/signup', methods=["GET"])
 def signup_get():
     return render_template('signup.html')
 
+# post
+
 @auth_blueprint.route('/signup', methods=["POST"])
 def signup_post():
     return redirect(url_for('home.index'))
+
+# logout route
+
+@auth_blueprint.route('/logout', methods=["DELETE"])
+def logout():
+    return render_template('logout.html')
