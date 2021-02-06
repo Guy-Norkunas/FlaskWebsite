e57c3132055d7f9704c33df0cb242b434aa0a8dd
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 21:07:02 +1030

    debugging in user controller

--- project/users/controller.py
+++ project/users/controller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import current_user, login_required
-from project.models import User
+from project.models import User, Movies
 
 users_blueprint = Blueprint(
     'users',
@@ -11,13 +11,14 @@ users_blueprint = Blueprint(
 @users_blueprint.route('/<id>', methods=["GET"])
 @login_required
 def index(id):
+    print(id)
     movies = []
 
     user_movies = Movies.query.filter_by(user_id=current_user.id).with_entities(Movies.movie_id).all()
     for user_movie in user_movies:
-        
+
 
         r = requests.get(f"https://api.themoviedb.org/3/movie/{user_movie[0]}?api_key=cd082f86556318fbd6e151825ae40fc7&language=en-US")
         movies.append(r.json())
 
-    return render_template('user.html', user=User.query.get(id), movies = )
+    return render_template('user.html', user=User.query.get(id), movies = movies)
