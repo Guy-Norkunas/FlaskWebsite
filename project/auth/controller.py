be9e16e8a5086c214ecb3144f12b84b4864a2a57
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-06 11:41:59 +1030

    added signup, hashed password

--- project/auth/controller.py
+++ project/auth/controller.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from project.models import User
+from project import db
 
 
 auth_blueprint = Blueprint(
@@ -37,12 +39,17 @@ def signup_post():
     username = request.form.get('username')
     password = request.form.get('password')
 
+    # check if the username is already taken
 
     if User.query.filter_by(username=username).first():
-        flash('username is already taken')
+        flash('Username is already taken')
         return redirect(url_for('auth.signup_get'))
-
-    return redirect(url_for('home.index'))
+    else:
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        flash(f'Account creation successful, logged in as {username}')
+        return redirect(url_for('home.index'))
 
 # logout route
 
