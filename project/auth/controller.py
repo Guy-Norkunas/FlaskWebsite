df5917ac377b2c99b3d49d964702868f51272594
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2021-02-07 23:29:43 +1030

    removing prints

--- project/auth/controller.py
+++ project/auth/controller.py
@@ -4,8 +4,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from project.models import User
 from project import db
 
-print("start of controller")
-
 auth_blueprint = Blueprint(
     'auth',
     __name__,
@@ -14,14 +12,10 @@ auth_blueprint = Blueprint(
 
 ## login routes
 
-print("after blueprint")
-
 # get
 
 @auth_blueprint.route('/login', methods=["GET"])
 def login_get():
-
-    print("start of login_get")
 
     if current_user.is_authenticated:
         flash('You are already logged in')
@@ -29,35 +23,22 @@ def login_get():
 
     return render_template('login.html')
 
-print("after /login GET route")
-
 # post
 
 @auth_blueprint.route('/login', methods=["POST"])
 def login_post():
 
-    print("start of login_post")
-
     if current_user.is_authenticated:
-
-        print("user is authenticatded")
 
         flash('You are already logged in')
         return redirect(url_for('home.index'))
 
-    print("before getting form stuff")
-
     username = request.form.get('username')
     password = request.form.get('password')
 
-    print("after getting form stuff")
-
     user = User.query.filter_by(username=username).first()
 
-    print("after doing a query")
-
     if user and check_password_hash(user.password, password):
-        print("check username and pass successful")
         login_user(user)
         return redirect(url_for('home.index'))
     else:
@@ -74,8 +55,6 @@ def login_post():
 @auth_blueprint.route('/signup', methods=["GET"])
 def signup_get():
 
-    print("signup_get")
-
     if current_user.is_authenticated:
         flash('You are already logged in')
         return redirect(url_for('home.index'))
@@ -87,30 +66,20 @@ def signup_get():
 @auth_blueprint.route('/signup', methods=["POST"])
 def signup_post():
 
-    print("signup_post")
-
     if current_user.is_authenticated:
         flash('You are already logged in')
         return redirect(url_for('home.index'))
 
-    print("before form stuff")
-
     username = request.form.get('username')
     password = request.form.get('password')
-
-    print("after form stuff")
 
     # check if the username is already taken
 
     if User.query.filter_by(username=username).first():
 
-        print("username taken")
-
         flash('Username is already taken')
         return redirect(url_for('auth.signup_get'))
     else:
-
-        print("registering new user")
 
         new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
         db.session.add(new_user)
